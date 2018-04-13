c17b06a5fb64d718671f872bd5afd246432929a4
Barney Whiteman <barney.whiteman@optusnet.com.au>
2018-04-13 12:52:17 +1000

    contact page clears boxes when a message is sent

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,5 +1,9 @@
 function send_email() {
 	var subject = document.getElementById('subject').value;
 	var body = document.getElementById('body').value;
+
+	document.getElementById('subject').value = '';
+	document.getElementById('body').value = '';
+
 	window.open("mailto:barney@barneyw.net?subject=" + subject + "&body=" + body);
 }
