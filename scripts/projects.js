6bae478631a670ac3a82145965409e781931f21d
Barney Whiteman <barney.whiteman@optusnet.com.au>
2018-06-14 19:24:43 +1000

    Updated project page to show gifs

---
+++ scripts/projects.js
@@ -0,0 +1,21 @@
+
+divs = [
+"<div style='position:relative;padding-bottom:54%'><iframe src='https://gfycat.com/ifr/UnfinishedRewardingAsianporcupine' frameborder='0' scrolling='no' width='100%' height='100%' style='position:absolute;top:0;left:0' allowfullscreen></iframe></div>",
+"<div style='position:relative;padding-bottom:54%'><iframe src='https://gfycat.com/ifr/VillainousBoldBumblebee' frameborder='0' scrolling='no' width='100%' height='100%' style='position:absolute;top:0;left:0' allowfullscreen></iframe></div>",
+"<div style='position:relative;padding-bottom:54%'><iframe src='https://gfycat.com/ifr/DeficientSpicyIslandwhistler' frameborder='0' scrolling='no' width='100%' height='100%' style='position:absolute;top:0;left:0' allowfullscreen></iframe></div>",
+"<div style='position:relative;padding-bottom:54%'><iframe src='https://gfycat.com/ifr/SevereDirectBaldeagle' frameborder='0' scrolling='no' width='100%' height='100%' style='position:absolute;top:0;left:0' allowfullscreen></iframe></div>",
+"<div style='position:relative;padding-bottom:54%'><iframe src='https://gfycat.com/ifr/LittleDelightfulInvisiblerail' frameborder='0' scrolling='no' width='100%' height='100%' style='position:absolute;top:0;left:0' allowfullscreen></iframe></div>",
+"<div style='position:relative;padding-bottom:54%'><iframe src='https://gfycat.com/ifr/MisguidedMajesticLamb' frameborder='0' scrolling='no' width='100%' height='100%' style='position:absolute;top:0;left:0' allowfullscreen></iframe></div>",
+"<div style='position:relative;padding-bottom:54%'><iframe src='https://gfycat.com/ifr/HugePrestigiousDalmatian' frameborder='0' scrolling='no' width='100%' height='100%' style='position:absolute;top:0;left:0' allowfullscreen></iframe></div>",
+"<div style='position:relative;padding-bottom:54%'><iframe src='https://gfycat.com/ifr/CoarseFloweryJapanesebeetle' frameborder='0' scrolling='no' width='100%' height='100%' style='position:absolute;top:0;left:0' allowfullscreen></iframe></div>",
+"<div style='position:relative;padding-bottom:54%'><iframe src='https://gfycat.com/ifr/PhysicalAcidicAmethystsunbird' frameborder='0' scrolling='no' width='100%' height='100%' style='position:absolute;top:0;left:0' allowfullscreen></iframe></div>",
+"<div style='position:relative;padding-bottom:54%'><iframe src='https://gfycat.com/ifr/VariableHonoredAdamsstaghornedbeetle' frameborder='0' scrolling='no' width='100%' height='100%' style='position:absolute;top:0;left:0' allowfullscreen></iframe></div>"
+]
+
+function loadGifs() {
+	var gifs = document.getElementById("gifs");
+	for(i in divs) {
+		gifs.innerHTML += divs[i];
+		gifs.innerHTML += "<hr class=\"my-4\">";
+	}
+}
