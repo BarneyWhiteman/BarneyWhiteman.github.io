cda0516294464687a65b376cb016415d0ab4d841
Barney Whiteman <barney.whiteman@optusnet.com.au>
2019-01-12 15:45:31 +1000

    Editted youtube page and added a gif to the projects page

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,21 +1,25 @@
 
 divs = [
-"<div style='position:relative;padding-bottom:54%'><iframe src='https://gfycat.com/ifr/UnfinishedRewardingAsianporcupine' frameborder='0' scrolling='no' width='100%' height='100%' style='position:absolute;top:0;left:0' allowfullscreen></iframe></div>",
-"<div style='position:relative;padding-bottom:54%'><iframe src='https://gfycat.com/ifr/VillainousBoldBumblebee' frameborder='0' scrolling='no' width='100%' height='100%' style='position:absolute;top:0;left:0' allowfullscreen></iframe></div>",
-"<div style='position:relative;padding-bottom:54%'><iframe src='https://gfycat.com/ifr/DeficientSpicyIslandwhistler' frameborder='0' scrolling='no' width='100%' height='100%' style='position:absolute;top:0;left:0' allowfullscreen></iframe></div>",
-"<div style='position:relative;padding-bottom:54%'><iframe src='https://gfycat.com/ifr/SevereDirectBaldeagle' frameborder='0' scrolling='no' width='100%' height='100%' style='position:absolute;top:0;left:0' allowfullscreen></iframe></div>",
-"<div style='position:relative;padding-bottom:54%'><iframe src='https://gfycat.com/ifr/LittleDelightfulInvisiblerail' frameborder='0' scrolling='no' width='100%' height='100%' style='position:absolute;top:0;left:0' allowfullscreen></iframe></div>",
-"<div style='position:relative;padding-bottom:54%'><iframe src='https://gfycat.com/ifr/MisguidedMajesticLamb' frameborder='0' scrolling='no' width='100%' height='100%' style='position:absolute;top:0;left:0' allowfullscreen></iframe></div>",
-"<div style='position:relative;padding-bottom:54%'><iframe src='https://gfycat.com/ifr/HugePrestigiousDalmatian' frameborder='0' scrolling='no' width='100%' height='100%' style='position:absolute;top:0;left:0' allowfullscreen></iframe></div>",
-"<div style='position:relative;padding-bottom:54%'><iframe src='https://gfycat.com/ifr/CoarseFloweryJapanesebeetle' frameborder='0' scrolling='no' width='100%' height='100%' style='position:absolute;top:0;left:0' allowfullscreen></iframe></div>",
-"<div style='position:relative;padding-bottom:54%'><iframe src='https://gfycat.com/ifr/PhysicalAcidicAmethystsunbird' frameborder='0' scrolling='no' width='100%' height='100%' style='position:absolute;top:0;left:0' allowfullscreen></iframe></div>",
-"<div style='position:relative;padding-bottom:54%'><iframe src='https://gfycat.com/ifr/VariableHonoredAdamsstaghornedbeetle' frameborder='0' scrolling='no' width='100%' height='100%' style='position:absolute;top:0;left:0' allowfullscreen></iframe></div>"
+"merryforsakenalbacoretuna",
+"UnfinishedRewardingAsianporcupine",
+"VillainousBoldBumblebee",
+"DeficientSpicyIslandwhistler",
+"SevereDirectBaldeagle",
+"LittleDelightfulInvisiblerail",
+"MisguidedMajesticLamb",
+"HugePrestigiousDalmatian",
+"CoarseFloweryJapanesebeetle",
+"PhysicalAcidicAmethystsunbird",
+"VariableHonoredAdamsstaghornedbeetle"
 ]
+
+start = "<div style='position:relative;padding-bottom:54%'><iframe src='https://gfycat.com/ifr/";
+end = "' frameborder='0' scrolling='no' width='100%' height='100%' style='position:absolute;top:0;left:0' allowfullscreen></iframe></div>";
 
 function loadGifs() {
 	var gifs = document.getElementById("gifs");
 	for(i in divs) {
-		gifs.innerHTML += divs[i];
+		gifs.innerHTML += start + divs[i] + end;
 		gifs.innerHTML += "<hr class=\"my-4\">";
 	}
 }
