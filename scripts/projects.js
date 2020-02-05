dbbe9e1b854d9bc122a47380dd0f6a54b76b96eb
Barney Whiteman <barney.whiteman@optusnet.com.au>
2020-02-05 21:05:37 +1000

    Added articles tab and changed the projects structure. Aded descriptions to all the images on the unexpected orcs project page.

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,4 +1,4 @@
-var start = "<div style='position:relative;padding-bottom:54%'><iframe src='https://gfycat.com/ifr/";
+var start = "<div class='gif' style='position:relative;padding-bottom:54%'><iframe src='https://gfycat.com/ifr/";
 var end = "' frameborder='0' scrolling='no' width='100%' height='100%' style='position:absolute;top:0;left:0' allowfullscreen></iframe></div>";
 
 var projects = [{"name": "Lissajous Curve", "url": "merryforsakenalbacoretuna"},
